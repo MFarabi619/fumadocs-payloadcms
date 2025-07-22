17f30fd817b78a95795327df7f0dfe338319b7aa
Mumtahin Farabi <mfarabi619@gmail.com>
2025-07-22 18:55:35 -0400

    refactor(docs): move to `(fumadocs)` subdir to be able to init payload
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- src/app/(fumadocs)/layout.tsx
+++ src/app/(fumadocs)/layout.tsx
@@ -1,4 +1,4 @@
-import '@/app/global.css';
+import '@/app/(fumadocs)/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
