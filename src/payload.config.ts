9eb51f9a5b54e41e30a6ba3a67d1ecc519dd46f2
Mumtahin Farabi <mfarabi619@gmail.com>
2025-07-24 15:11:17 -0400

    build(env,payload): update setup to make `.env` unnecessary for local dev
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,4 @@
 import { sqliteAdapter } from '@payloadcms/db-sqlite'
-// import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -21,26 +20,25 @@ export default buildConfig({
   },
   collections: [Users, Media],
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
+  secret: process.env.NODE_ENV === "development" ? 'YOUR_SECRET_HERE' : process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
-  serverURL: process.env.NODE_ENV !== "development" && process.env.PUBLIC_URL || '',
+  serverURL: process.env.NODE_ENV === "development" ? 'http://localhost:3000' : process.env.BASE_URL,
   cors: process.env.CORS_WHITELIST_ORIGINS
     ? process.env.CORS_WHITELIST_ORIGINS.split(",")
-    : [],
+    : ["http://localhost:3000"],
   csrf: process.env.CSRF_WHITELIST_ORIGINS
     ? process.env.CSRF_WHITELIST_ORIGINS.split(",")
-    : [],
+    : ["http://localhost:3000"],
   db: sqliteAdapter({
     client: {
-      url: process.env.DATABASE_URI || process.env.TURSO_DATABASE_URL || '',
-      authToken: process.env.AUTH_TOKEN || process.env.TURSO_AUTH_TOKEN || '',
+      url: process.env.NODE_ENV === "development" ? "file:./sqlite.db" : process.env.DATABASE_URI || process.env.TURSO_DATABASE_URL || '',
+      authToken: process.env.NODE_ENV === "development" ? "" : process.env.AUTH_TOKEN || process.env.TURSO_AUTH_TOKEN || '',
     },
   }),
   sharp,
   plugins: [
-    // payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
 })
