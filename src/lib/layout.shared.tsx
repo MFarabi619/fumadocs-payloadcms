3c1f260be53277ddb44fa8b4e816824972117256
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-20 17:48:57 -0500

    build(nextjs): merge configs
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,9 +1,34 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image'
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'My App',
+      title: (
+        <>
+          <picture>
+            <source srcSet="/fumadocs.svg" />
+            <Image alt="Fumadocs Logo Symbol" height={35} src="/fumadocs.svg" width={35} />
+          </picture>
+          +
+          <picture>
+            <source srcSet="/payload-favicon.svg" />
+            <Image alt="Payload Logo" height={35} src="/payload-favicon.svg" width={35} />
+          </picture>
+        </>
+      ),
     },
-  };
+    // see https://fumadocs.dev/docs/ui/navigation/links
+    links: [
+      {
+        text: 'Docs',
+        url: '/docs',
+      },
+      {
+        text: 'Admin',
+        url: '/admin',
+      },
+    ],
+    githubUrl: 'https://github.com/MFarabi619/fumadocs-payloadcms',
+  }
 }
