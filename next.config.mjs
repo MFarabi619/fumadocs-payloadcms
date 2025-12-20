f69383e029a20cf59241b3b37e03bbe13604f3bc
Mumtahin Farabi <mfarabi619@gmail.com>
2025-12-20 17:48:57 -0500

    build(nextjs): merge configs
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,32 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { withPayload } from '@payloadcms/next/withPayload'
+import { createMDX } from 'fumadocs-mdx/next'
 
-const withMDX = createMDX();
+const withMDX = createMDX()
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-};
+  // TODO: invesigate...
+  // ./src/collections/Users.ts:79:26
+  // Type error: Property 'id' does not exist on type 'File'.
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (webpackConfig) => {
+    webpackConfig.resolve.extensionAlias = {
+      '.cjs': ['.cts', '.cjs'],
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+      '.mjs': ['.mts', '.mjs'],
+    }
 
-export default withMDX(config);
+    return webpackConfig
+  },
+}
+
+// export default withMDX(config);
+// export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(withMDX(config), { devBundleServerPackages: false })
